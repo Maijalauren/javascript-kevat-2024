cc33e6a5a91da37911f9c0d8e522a1203ddc8aa2
Maija Lauren <maija.e.lauren@gmail.com>
2024-01-17 11:03:32 +0200

    buttonit

---
+++ sivu.js
@@ -0,0 +1,14 @@
+
+// Palvelut sivun näyttäminen
+function palvelut() {
+let palvelut = `<h2>Palvelumme<h2>
+<ul>
+        <li>Eläinlääkäri</li>
+        <li>Päivähoito</li>
+        <li>Yöhoito</li>
+
+</ul>
+`
+document.getElementById("sisältö").innerHTML = palvelut
+
+}
